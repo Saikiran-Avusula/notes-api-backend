e9574d4c2187b2a14116b0acb91a5a7d24a6e35b
Saikiran-Avusula <saikiranavusula89@gmail.com>
2025-12-05 18:41:34 +0530

    updated files

--- src/main/java/com/notesapi/security/CustomUserDetailService.java
+++ src/main/java/com/notesapi/security/CustomUserDetailService.java
@@ -1,7 +1,7 @@
 package com.notesapi.security;
 
 import com.notesapi.model.User;
-import com.notesapi.repositroy.UserRepository;
+import com.notesapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
